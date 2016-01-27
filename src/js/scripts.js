2da2eb50498f509685d2a9107c33f91b1cb80a78
Joshua Carrafa <carrafa@gmail.com>
2016-01-27 09:01:57 -0500

    adds fish generator, tweaks animations

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,21 +3,35 @@ console.log('hi, nosy person.');
 $(function() {
   scrollListener();
   batmanify($('.profile-pic'), '../images/josh.png', '-50%');
+  fishListener();
 });
 
+function fishListener() {
+  $('body').click(function() {
+    makeAFish();
+  });
+}
 
-function makeAFish(el) {
+function makeAFish() {
   $body = $('<div>').addClass('body');
   $eye = $('<div>').addClass('eye');
   $tail = $('<div>').addClass('tail');
   $fishContainer = $('<div>').addClass('fish-container');
 
+  var topOffset = Math.floor(Math.random() * 184) - 19;
+
+  $fishContainer.css({
+    top: topOffset + "em"
+  });
   $body.append($eye);
   $fishContainer.append($body);
   $fishContainer.append($tail);
 
-  el.append($fishContainer);
+  $('.acquarium').append($fishContainer);
+
 }
+
+// fish top range: -19em to 165em
 
 function batmanify(el, image, offset) {
   el.batmanify({
